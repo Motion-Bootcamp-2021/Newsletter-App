1bbf605d1784f2ec40f3c32501d0461c8e9aefa2
Ivo Manov <ivoivchov@gmail.com>
2021-04-07 15:00:40 +0300

    Initialize ipetrov22/issue310

---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,38 @@
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import LandingHeader from './LandingHeader';
+import LandingFeatures from './LandingFeatures';
+import LandingLetterboxDescription from './LandingLetterboxDescription';
+import LandingCategories from './LandingCategories';
+import LandingHappyUsers from './LandingHappyUsers';
+import LandingSubscribe from './LandingSubscribe';
+import LandingFooter from './LandingFooter';
+import './Landing.scss'
+
+const Landing = ({ user }) => {
+    let history = useHistory();
+
+    if (user) {
+        history.push('/explore-feeds');
+    }
+
+    return (
+        <div className="landing-wrapper">
+            <div className="landing">
+                <LandingHeader />
+                <LandingFeatures />
+                <LandingLetterboxDescription />
+                <LandingCategories />
+                <LandingHappyUsers />
+                <LandingSubscribe />
+                <LandingFooter />
+            </div>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({
+    user: state.user.user,
+});
+
+export default connect(mapStateToProps, null)(Landing);
